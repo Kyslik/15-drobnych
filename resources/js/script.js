acf988eb0a1a649b280421ba0ff732f59081f941
Martin Kiesel <martin.kiesel@gmail.com>
2014-04-25 22:44:21 +0200

    Added heartbeat effect.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,26 @@
  $(function(){
-
 	$('ul#difficulty li').click(function() {
 		$('li.active').removeClass('active');
 		$(this).addClass('active');
 	});
+});
 
+
+function pulse() {
+    $('button.heartbeat').animate({
+        width: 200, height: 200, 
+        'font-size': 50,
+        opacity: 0.5
+    }, 700, function() {
+        $('button.heartbeat').animate({
+            width: 220, height: 220, 
+            'font-size': 55,
+            opacity: 1
+        }, 700, function() {
+            pulse();
+        });
+    }); 
+};
+$( document ).ready(function() {
+    pulse();
 });

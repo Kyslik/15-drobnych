79c0ebe1ca568932036b840e1d76da91275b3b90
Martin Kiesel <martin.kiesel@gmail.com>
2014-04-26 00:55:55 +0200

    Added screens, spacebar functionality.

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,3 +1,69 @@
 var game_background = document.getElementById('game-background');
 var game_menu = document.getElementById('game-menu');
 var game_menu_repeat = document.getElementById('game-menu-repeat');
+
+
+
+
+
+function checkKeyDown(e) {
+
+    var keyID = e.keyCode || e.which;
+
+/*    if (keyID === 38 || keyID === 87) { //up arrow or W key
+        isUpKey = true;
+        e.preventDefault();
+    }*/
+
+    if (keyID === 39 || keyID === 68) { //right arrow or D key
+        isRightKey = true;
+        e.preventDefault();
+    }
+
+/*    if (keyID === 40 || keyID === 83) { //down arrow or S key
+        isDownKey = true;
+        e.preventDefault();
+    }*/
+
+    if (keyID === 37 || keyID === 65) { //left arrow or A key
+        isLeftKey = true;
+        e.preventDefault();
+    }
+
+/*    if (keyID === 32) { //spacebar
+        isSpacebar = true;
+        e.preventDefault();
+    }*/
+
+}
+
+function checkKeyUp(e) {
+
+    var keyID = e.keyCode || e.which;
+
+/*    if (keyID === 38 || keyID === 87) { //up arrow or W key
+        isUpKey = false;
+        e.preventDefault();
+    }*/
+
+    if (keyID === 39 || keyID === 68) { //right arrow or D key
+        isRightKey = false;
+        e.preventDefault();
+    }
+
+/*    if (keyID === 40 || keyID === 83) { //down arrow or S key
+        isDownKey = false;
+        e.preventDefault();
+    }*/
+
+    if (keyID === 37 || keyID === 65) { //left arrow or A key
+        isLeftKey = false;
+        e.preventDefault();
+    }
+
+/*    if (keyID === 32) { //spacebar
+        isSpacebar = false;
+        e.preventDefault();
+    }*/
+
+}
--- resources/js/script.js
+++ resources/js/script.js
@@ -5,22 +5,96 @@
 	});
 });
 
+var pulse_on = true;
+var actual_screen = "menu";
 
 function pulse() {
+	if (!pulse_on) return;
     $('button.heartbeat').animate({
         width: 200, height: 200, 
         'font-size': 50,
+        top: 50,
         opacity: 0.5
     }, 700, function() {
         $('button.heartbeat').animate({
             width: 220, height: 220, 
             'font-size': 55,
+            top: 40,
             opacity: 1
         }, 700, function() {
             pulse();
         });
     }); 
 };
+
+function displayGameMenu() {
+	pulse_on = true;
+	pulse();
+
+	actual_screen = "menu";
+
+	$('#game-menu').css("display", "block");
+	$('#game-menu-repeat').css("display", "none");
+	$('#game-board').css("display", "none");
+	$('button.spacebar-btn').removeClass('active');
+
+}
+
+function displayGameRepeat() {
+	pulse_on = true;
+	pulse();
+
+	actual_screen = "repeat";
+
+	$('#game-menu').css("display", "none");
+	$('#game-menu-repeat').css("display", "block");
+	$('#game-board').css("display", "none");
+	$('button.spacebar-btn').removeClass('active');
+}
+
+function displayGame() {
+
+	actual_screen = "game";
+
+	$('#game-menu').css("display", "none");
+	$('#game-menu-repeat').css("display", "none");
+	$('#game-board').css("display", "block");
+}
+
+function changeScreen(display) {
+	$('#game-menu-change').css("display", "block");
+	$('#game-menu-change').animate({
+		opacity: 1,
+	}, 600, function() {
+
+		if (display == "game") displayGame();
+		if (display == "menu") displayGameMenu();
+		if (display == "repeat") displayGameRepeat();
+		
+		$(this).animate({
+			opacity: 0,
+		}, 600, function() {
+			$('#game-menu-change').css("display", "none");
+		});
+	});
+}
+
 $( document ).ready(function() {
     pulse();
+
+    $('button.go, button.spacebar-btn').click(function() {
+    	pulse_on = false;
+    	changeScreen('game');
+	});
+
 });
+
+if (actual_screen == "menu" || actual_screen == "repeat") {
+	$(window).keypress(function(e) {
+	  if (e.keyCode == 32) {
+	  	e.preventDefault();
+	  	console.log("spacebar");
+	    $('button.spacebar-btn').addClass('active').click();
+	  }
+	});
+}

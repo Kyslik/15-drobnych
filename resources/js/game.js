c1c2f0d95aba3291c8b4022b72734754cf4138ad
Martin Kiesel <martin.kiesel@gmail.com>
2014-04-25 20:21:40 +0200

    Initial commit from local host.

---
+++ resources/js/game.js
@@ -0,0 +1,3 @@
+var game_background = document.getElementById('game-background');
+var game_menu = document.getElementById('game-menu');
+var game_menu_repeat = document.getElementById('game-menu-repeat');
